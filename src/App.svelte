a4afd9908bc02e7eb9e8c8f12c9d347131d96a4a
Jorge Anton <jorgeanton1810@gmail.com>
2024-11-18 12:03:04 +0100

    Git

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Buscador from './lib/Buscador.svelte'
+  import Counter from './lib/Counter.svelte'
+  import Modo from './lib/Modo.svelte'
+  import Correo from './lib/Correo.svelte'
+    import Listatareas from './lib/listatareas.svelte';
+    import Pantallafigma from './lib/pantallafigma.svelte';
+</script>
+
+<main>
+  
+ 
+  
+  <div class="card">
+   <!-- <Counter />
+    <Buscador />
+    <Modo />
+    <Correo />
+    <Listatareas/> -->
+    <Pantallafigma/>
+    
+  </div>
+
+</main>
+
+<style>
+
+</style>
